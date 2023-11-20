1aa821591ad6677b469113bba346ee43cd5b8dc0
d-komissarchik <0667669609@ukr.net>
2023-11-20 16:21:01 +0200

    Added adding categories

--- handlers/admin/add.py
+++ handlers/admin/add.py
@@ -3,6 +3,10 @@ from filters import IsAdmin
 from handlers.user.menu import settings
 from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.callback_data import CallbackData
+from aiogram.types import CallbackQuery
+from states import CategoryState
+from hashlib import md5
+from aiogram.dispatcher import FSMContext
 
 category_cb = CallbackData('category', 'id', 'action')
 
@@ -21,3 +25,22 @@ async def process_settings(message: Message):
         '+ Додати категорію', callback_data='add_category'))
 
     await message.answer('Налаштування категорій:', reply_markup=markup)
+
+
+@dp.callback_query_handler(IsAdmin(), text='add_category')
+async def add_category_callback_handler(query: CallbackQuery):
+    await query.message.delete()
+    await query.message.answer('Введіть назву категорії')
+    await CategoryState.title.set()
+
+
+
+@dp.message_handler(IsAdmin(), state=CategoryState.title)
+async def set_category_title_handler(message: Message, state: FSMContext):
+
+    category = message.text
+    idx = md5(category.encode('utf-8')).hexdigest()
+    db.query('INSERT INTO categories VALUES (?, ?)', (idx, category))
+
+    await state.finish()
+    await process_settings(message)
