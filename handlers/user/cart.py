7b04adc4c30955d5e425c8cfec4f16a8bfd2cbe6
d-komissarchik <0667669609@ukr.net>
2023-11-30 12:42:44 +0200

    Added cart

--- handlers/user/cart.py
+++ handlers/user/cart.py
@@ -19,4 +19,23 @@ async def process_cart(message: Message, state: FSMContext):
         await bot.send_chat_action(message.chat.id, ChatActions.TYPING)
         async with state.proxy() as data:
             data['products'] = {}
-
+        order_cost = 0
+        for _, idx, count_in_cart in cart_data:
+            product = db.fetchone('SELECT * FROM products WHERE idx=?', (idx,))
+            if product == None:
+                db.query('DELETE FROM cart WHERE idx=?', (idx,))
+            else:
+                _, title, body, image, price, _ = product
+                order_cost += price
+                async with state.proxy() as data:
+                    data['products'][idx] = [title, price, count_in_cart]
+                markup = product_markup(idx, count_in_cart)
+                text = f'<b>{title}</b>\n\n{body}\n\n Ціна: {price} гривень.'
+                await message.answer_photo(photo=image,
+                                           caption=text,
+                                           reply_markup=markup)
+        if order_cost != 0:
+            markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+            markup.add('📦 Оформити замовлення')
+            await message.answer('Перейти до оформлення?',
+                                 reply_markup=markup)
