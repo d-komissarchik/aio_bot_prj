03e238009d1cc20865e7761d3d73cdcfce2a7bc0
d-komissarchik <0667669609@ukr.net>
2023-12-15 14:08:19 +0200

    Added sos

---
+++ handlers/user/sos.py
@@ -0,0 +1,17 @@
+from aiogram.dispatcher import FSMContext
+from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove
+from keyboards.default.markups import all_right_message, cancel_message, \
+    submit_markup
+from aiogram.types import Message
+from states import SosState
+from filters import IsUser
+from loader import dp, db
+
+
+@dp.message_handler(commands='sos')
+async def cmd_sos(message: Message):
+    await SosState.question.set()
+    await message.answer(
+        "Опишіть якомога детальніше суть проблеми "
+        "та адміністратор обов'язково вам відповість якнайшвидше",
+        reply_markup=ReplyKeyboardRemove())
