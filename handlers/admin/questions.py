c6f32f5c344fc383a274bc8c6badd6fb2268a7b4
d-komissarchik <0667669609@ukr.net>
2023-12-19 14:03:22 +0200

    Added admin questions

--- handlers/admin/questions.py
+++ handlers/admin/questions.py
@@ -10,22 +10,16 @@ from filters import IsAdmin
 
 question_cb = CallbackData('question', 'cid', 'action')
 
-
 @dp.message_handler(IsAdmin(), text=questions)
 async def process_questions(message: Message):
     await bot.send_chat_action(message.chat.id, ChatActions.TYPING)
     questions = db.fetchall('SELECT * FROM questions')
-
     if len(questions) == 0:
-
         await message.answer('Немає питань.')
-
     else:
-
         for cid, question in questions:
             markup = InlineKeyboardMarkup()
             markup.add(InlineKeyboardButton(
                 'Відповісти',
                 callback_data=question_cb.new(cid=cid, action='answer')))
-
             await message.answer(question, reply_markup=markup)
