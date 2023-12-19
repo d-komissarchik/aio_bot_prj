6a26962cc78220ada49bdeced17fa95d074ff8cc
d-komissarchik <0667669609@ukr.net>
2023-12-19 14:01:44 +0200

    Added admin questions

--- handlers/admin/questions.py
+++ handlers/admin/questions.py
@@ -1,5 +1,14 @@
+from handlers.user.menu import questions
+from aiogram.dispatcher import FSMContext
+from aiogram.utils.callback_data import CallbackData
+from keyboards.default.markups import all_right_message, cancel_message, submit_markup
+from aiogram.types import Message, CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardRemove
+from aiogram.types.chat import ChatActions
 
+from loader import dp, db, bot
+from filters import IsAdmin
 
+question_cb = CallbackData('question', 'cid', 'action')
 
 
 @dp.message_handler(IsAdmin(), text=questions)
